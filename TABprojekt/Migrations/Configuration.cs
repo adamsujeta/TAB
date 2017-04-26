e732a8c436e796c060aba00a7f0c596179c5a332
Adam Sujeta <adamsujeta@gmail.com>
2017-04-26 14:48:15 +0200

    fix

--- TABprojekt/Migrations/Configuration.cs
+++ TABprojekt/Migrations/Configuration.cs
@@ -5,14 +5,14 @@ namespace TABprojekt.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<TABprojekt.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<TABprojekt.Models.TABprojektContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(TABprojekt.Models.ApplicationDbContext context)
+        protected override void Seed(TABprojekt.Models.TABprojektContext context)
         {
             //  This method will be called after migrating to the latest version.
 
